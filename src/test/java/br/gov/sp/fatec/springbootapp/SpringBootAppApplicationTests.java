fb857f41cefceb35da48778ec014acde816b6821
GRCursino <guiiix93@gmail.com>
2020-09-16 03:18:47 +0000

    Subindo JPA

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,36 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
+
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class SpringBootAppApplicationTests {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
 	}
+
+    @Test
+    void testeInsercao(){
+        
+        Usuario usuario = new Usuario();
+        usuario.setNome("Guilherme");
+        usuario.setSenha("senha");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+
+    }
 
 }
